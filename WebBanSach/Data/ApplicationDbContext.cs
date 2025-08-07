af86ec2cff469c191ac8b9a0c42172d10c60574c
tuanmanh245xz <tuanmanh435@gmail.com>
2025-08-07 17:00:37 +0700

    Add project files.

---
+++ WebBanSach/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebBanSach.Models;
+
+namespace WebBanSach.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ WebBanSach/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBanSach.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+      
+    }
+}
