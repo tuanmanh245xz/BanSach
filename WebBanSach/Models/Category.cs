4847265a6a8d7cb952f86e2c2acf4b99d33dda5d
tuanmanh245xz <tuanmanh435@gmail.com>
2025-08-08 10:42:41 +0700

    them annotation Ranger

--- WebBanSach/Models/Category.cs
+++ WebBanSach/Models/Category.cs
@@ -9,6 +9,8 @@ namespace WebBanSach.Models
         [Required]
         public string Name { get; set; }
         [Display(Name="Display Order")]
+        [Range(10, 1000,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
       
